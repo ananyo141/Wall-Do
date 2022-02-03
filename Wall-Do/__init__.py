061f0d175ff6145100d0d05f40881feb808f7d0d
Ananyobrata Pal <ananyo141@gmail.com>
2022-02-03 20:45:07 +0530

    Initial Commit

---
+++ Wall-Do/__init__.py
@@ -0,0 +1,7 @@
+"
+Wall-Do! - A Wallpaper Downloader written in python and tkinter
+
+Author: Ananyobrata Pal
+Date:   03/02/2022
+"
+
---
+++ Wall-Do/downloader.py
@@ -0,0 +1,6 @@
+# This module contains all the required backend stuff (logic)
+# required to handle and download from website.
+
+# URL signature looks like:
+# https://wall.alphacoders.com/search.php?search={searchKey}&page={PageNo}
+
