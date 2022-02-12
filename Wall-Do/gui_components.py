dd19fc380e6293439f7c7f822ae82fed14d4f22e
ananyo141 <ananyo141@gmail.com>
2022-02-13 00:54:35 +0530

    add to gui body

--- Wall-Do/gui_components.py
+++ Wall-Do/gui_components.py
@@ -1,19 +1,18 @@
 """
- This file contains the GUI components that make 
+ This module contains the GUI components that make 
  up the interface for Wall-Do
 
 """
 
 # Wildcard imports are fine as this module deals with only
 # tk widgets; use namespaces in the main script
-import sys
+import sys, os
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox as msgb, filedialog as fldg
 from exceptions import TopLevelWidgetsOnly
 
-
-# Create the menu for the main window; May need to use the more
+# Create the menu for the main window; may need to use the more
 # specialized menu framework if menu needs to be created in other
 # windows too
 class MakeMenu:
@@ -53,7 +52,7 @@ class MakeMenu:
 
     def aboutDialog(self):
         msgb.showinfo(title='About', message='Author: Ananyobrata Pal\n'
-                                             'Licensed Under MIT License\n'
+                                             'Licensed Under: MIT License\n'
                                              '(c) Copyright: 2022-present')
 
     def importFile(self):
@@ -71,8 +70,60 @@ class MakeMenu:
     def resumeEdit(self):
         raise NotImplementedError
 
+# Reusable Frame components
+# Make up the gui input body
+class GuiInput(Frame):
+    entryWidgetWidth = 30
+    padding = dict(
+        padx=4,
+        pady=4
+    )
+    def __init__(self, parent, **kw):
+        Frame.__init__(self, master=parent, **kw)
+        self.dirVar = StringVar(value=os.getcwd())
+        self.searchVar = StringVar(value='Enter Search Key here')
+        self.numImageVar = IntVar(value=30)
+
+        self.makeDirInput()
+        self.makeSearchInput()
+        self.makeNumImageInput()
+    
+    def makeDirInput(self):
+        def chooseDir():
+            inpDir = fldg.askdirectory(title='Enter directory')
+            if inpDir:
+                self.dirVar.set(os.path.normpath(inpDir))
+
+        dirFrame = Frame(self)
+        dirFrame.pack(expand=True, fill=BOTH)
+        Label(dirFrame, text='Directory:').pack(side=LEFT, **self.padding)
+        Entry(dirFrame, textvariable=self.dirVar, 
+                width=self.entryWidgetWidth).pack(side=LEFT, expand=True, fill=X, **self.padding)
+
+        Button(dirFrame, text='Browse', command=chooseDir, 
+                width=7).pack(side=RIGHT, **self.padding)
+
+    def makeSearchInput(self):
+        (searchFrame := Frame(self)).pack(expand=True, fill=BOTH)
+        Label(searchFrame, text='Search: ').pack(side=LEFT, **self.padding)
+        Entry(searchFrame, textvariable=self.makeSearchInput,
+            width=self.entryWidgetWidth).pack(side=LEFT, **self.padding)
+
+
+    def makeNumImageInput(self):
+        pass
+
+# Downloader Info
+class GuiDetails(Frame):
+    pass
+
+# Image Viewer
+class GuiImgViewer(Frame):
+    pass
+
 if __name__ == '__main__':
     root = Tk()
     root.title("Tester")
     MakeMenu(root)
+    GuiInput(root).pack(expand=True, fill=BOTH)
     mainloop()
